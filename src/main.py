1c42f747991f3b6798607768b156a12160677715
Markus Telser <markus.telser@gmail.com>
2021-07-01 16:00:58 +0200

    change paths relative

--- src/main.py
+++ src/main.py
@@ -5,20 +5,30 @@ from time import sleep
 from json import load, dump
 from os.path import isfile
 from os import stat, getcwd, listdir
-from os.path import join, isdir
+from os.path import join, isdir, dirname
+from sys import modules
 
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchWindowException
 
+ROOT_DIR = dirname(dirname(modules['__main__'].__file__))
+
+def write_data(website, title, price, location, date, link):
+    data_path = join(ROOT_DIR, "data", website + ".txt")
+    with open(data_path, "a") as f:
+        f.write(f"{title} || {price} || {location} || {date} || {link}\n")
+
+
 def save_latest_entry(website, link):
+    history_path = join(ROOT_DIR, "data","history.json")
     oldEntry = ""
     # get saved data
     data = {}
-    if not isfile("history.json") or stat("history.json").st_size == 0:
-        with open("history.json", 'w'):
+    if not isfile(history_path) or stat(history_path).st_size == 0:
+        with open(history_path, 'w'):
             pass
     else:
-        with open("history.json") as f:
+        with open(history_path) as f:
             data = load(f)
     if website in data:
         oldEntry = data[website]
@@ -26,64 +36,126 @@ def save_latest_entry(website, link):
     new_data = {website : link}
     data.update(new_data)
     # append new data
-    with open("../data/history.json", "w") as f:
+    
+    with open(history_path, "w") as f:
         dump(data, f)
     return oldEntry
     
-
-def gen_website():
+def gen_html():
     html = '<!DOCTYPE html><html>'
     html += '<head><meta charset="utf-16"><title>New Items</title></head>'
-    html += """
-            <style>
-            table{
-                width: 100%;
-            }
-            table, th, td{
-                border: 2px solid green;
-            }
-            </style>
-            """
-    html += '<body><h1>New Items</h1>'
-    if isdir(".newitems"):
-        html += '<table id="table">'
-        for i in range(len(listdir(".newitems"))):
-            html += '<tr><th colspan="4">'+ listdir(".newitems")[i].strip() +'</th></tr>'
-            with open(join(".newitems",listdir(".newitems")[i]), "r") as f:
+    html += '<link rel="stylesheet" href="style.css">'
+    html += '<script src="script.js"></script>'
+    html += '<body><h1>New Marketplace Items</h1>'
+    if isdir("data"):
+        
+        websites = listdir(join(ROOT_DIR, "data", "websites"))
+        for i in range(len(websites)):
+            html += '<table class="section">'
+            html += '<tr class="site"><th colspan="4">'+ websites[i].split('.')[0] +'</th></tr>'
+            with open(join(ROOT_DIR, "data", "websites", websites[i]), "r") as f:
                 lines = f.readlines()
-                html += f'<tr><td style="width:25%">Titel</a></td>'
-                html += f'<td style="width:25%">Price</td>'
-                html += f'<td style="width:25%">Location</td>'
-                html += f'<td style="width:25%">Date</td></tr>'
+                html += f'<tr><td class="type">Titel</a></td>'
+                html += f'<td class="type">Price</td>'
+                html += f'<td class="type">Location</td>'
+                html += f'<td class="type">Date</td></tr>'
                 # add all items
                 for line in lines:
                     items = line.split("||")
-                    html += f'<tr><td style="width:25%"><a href="{items[4]}">{items[0]}</a></td>'
-                    html += f'<td style="width:25%">{items[1]}</td>'
-                    html += f'<td style="width:25%">{items[2]}</td>'
-                    html += f'<td style="width:25%">{items[3]}</td></tr>'
+                    html += f'<tr><td class="items"><a href="{items[4]}">{items[0]}</a></td>'
+                    html += f'<td class="items">{items[1]}</td>'
+                    html += f'<td class="items">{items[2]}</td>'
+                    html += f'<td class="items">{items[3]}</td>'
+                    html += '<td><input type="button" value="Delete" onclick="deleteRow(this)"/></td></tr>'
+            html += '</table>'
     html += '</body></html>'
+    return html
 
-    with open("index.html", "w") as f:
-        f.write(html)
+def gen_js():
+    js = """
+    function deleteRow(btn) {
+        var row = btn.parentNode.parentNode;
+        row.parentNode.removeChild(row);
+    }
+    """
+    return js
 
+def gen_css():
+    css = """
+    body{
+        background-color: #E1E8ED;
+    }
+    h1{
+        font-size: 30pt;
+        font-color: #292F33;
+        justify-content: center;
+        text-align: center;
+    }
+    table{
+        margin-left: auto;
+        margin-right: auto;
+        width: 80%;
+    }
+    th, td{
+        border: 2px solid  #66757F;
+        font-color: #CCD6DD;
+    }
+    th{
+        font-size: 22pt;
+        padding: 5px;
+    }
+    a {
+        color: #66757F;
+    }
+    td{
+        width: 25%;
+        font-size: 16pt;
+    }
+    .section{
+        margin-top: 30px;
+    }
+    .type{
+        font-size: 20pt;
+        font-weight: bold;
+    }
+    .items{
+        width: 20%;
+    }
+    """
+    return css
+
+def gen_website():
+    # write html file
+    with open(join(ROOT_DIR, "page", "index.html"), "w") as f:
+        f.write(gen_html())
+    
+    # write css file
+    with open(join(ROOT_DIR, "page", "style.css"), "w") as f:
+        f.write(gen_css())
+
+    # write js file
+    with open(join(ROOT_DIR, "page", "script.js"), "w") as f:
+        f.write(gen_js())
+    
+    
 def open_website(driver):
-    path = join(getcwd(),"index.html")
+    path = join(ROOT_DIR, "page", "index.html")
     driver.get("file:///" + path)
     sleep(100)
 
 def main():
     try:
         options = webdriver.ChromeOptions()
+        options.add_experimental_option("detach", True) # stop closing
         options.add_experimental_option('excludeSwitches', ['enable-logging']) # disable logging
 
         driver = webdriver.Chrome(options=options)
 
         # fetch new data from websites
         #web.second_hand(driver)
-        #web.subito(driver, min_price=3000, last_page=2)
-        web.facebook_marketplace(driver)
-        sleep(100)
+        #web.subito(driver, last_page=2)
+        #web.facebook_marketplace(driver)
+
         # generate showcase website, open
         gen_website()
         open_website(driver)
