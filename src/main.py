5d99d7e1dc913b87bf340f6d61bb305d5f10c5e0
Markus Telser <markus.telser@gmail.com>
2021-07-12 10:12:51 +0200

    outsourced gen website, append data beginning

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 import websites as web
+from page import gen_website
 
 from genericpath import isfile
 from time import sleep
 from json import load, dump
 from os.path import isfile
-from os import stat, getcwd, listdir
-from os.path import join, isdir, dirname
+from os import stat
+from os.path import join, dirname
 from sys import modules
 
 from selenium import webdriver
@@ -14,9 +15,13 @@ from selenium.common.exceptions import NoSuchWindowException
 ROOT_DIR = dirname(dirname(modules['__main__'].__file__))
 
 def write_data(website, title, price, location, date, link):
-    data_path = join(ROOT_DIR, "data", website + ".txt")
-    with open(data_path, "a") as f:
+    data_path = join(ROOT_DIR, "data", "websites", website + ".txt")
+    with open(data_path, "r") as f:
+        data = f.read()
+    with open(data_path, "w") as f:
         f.write(f"{title} || {price} || {location} || {date} || {link}\n")
+    with open(data_path, "a") as f:
+        f.write(data)
 
 
 def save_latest_entry(website, link):
@@ -41,103 +46,6 @@ def save_latest_entry(website, link):
         dump(data, f)
     return oldEntry
     
-def gen_html():
-    html = '<!DOCTYPE html><html>'
-    html += '<head><meta charset="utf-16"><title>New Items</title></head>'
-    html += '<link rel="stylesheet" href="style.css">'
-    html += '<script src="script.js"></script>'
-    html += '<body><h1>New Marketplace Items</h1>'
-    if isdir("data"):
-        
-        websites = listdir(join(ROOT_DIR, "data", "websites"))
-        for i in range(len(websites)):
-            html += '<table class="section">'
-            html += '<tr class="site"><th colspan="4">'+ websites[i].split('.')[0] +'</th></tr>'
-            with open(join(ROOT_DIR, "data", "websites", websites[i]), "r") as f:
-                lines = f.readlines()
-                html += f'<tr><td class="type">Titel</a></td>'
-                html += f'<td class="type">Price</td>'
-                html += f'<td class="type">Location</td>'
-                html += f'<td class="type">Date</td></tr>'
-                # add all items
-                for line in lines:
-                    items = line.split("||")
-                    html += f'<tr><td class="items"><a href="{items[4]}">{items[0]}</a></td>'
-                    html += f'<td class="items">{items[1]}</td>'
-                    html += f'<td class="items">{items[2]}</td>'
-                    html += f'<td class="items">{items[3]}</td>'
-                    html += '<td><input type="button" value="Delete" onclick="deleteRow(this)"/></td></tr>'
-            html += '</table>'
-    html += '</body></html>'
-    return html
-
-def gen_js():
-    js = """
-    function deleteRow(btn) {
-        var row = btn.parentNode.parentNode;
-        row.parentNode.removeChild(row);
-    }
-    """
-    return js
-
-def gen_css():
-    css = """
-    body{
-        background-color: #E1E8ED;
-    }
-    h1{
-        font-size: 30pt;
-        font-color: #292F33;
-        justify-content: center;
-        text-align: center;
-    }
-    table{
-        margin-left: auto;
-        margin-right: auto;
-        width: 80%;
-    }
-    th, td{
-        border: 2px solid  #66757F;
-        font-color: #CCD6DD;
-    }
-    th{
-        font-size: 22pt;
-        padding: 5px;
-    }
-    a {
-        color: #66757F;
-    }
-    td{
-        width: 25%;
-        font-size: 16pt;
-    }
-    .section{
-        margin-top: 30px;
-    }
-    .type{
-        font-size: 20pt;
-        font-weight: bold;
-    }
-    .items{
-        width: 20%;
-    }
-    """
-    return css
-
-def gen_website():
-    # write html file
-    with open(join(ROOT_DIR, "page", "index.html"), "w") as f:
-        f.write(gen_html())
-    
-    # write css file
-    with open(join(ROOT_DIR, "page", "style.css"), "w") as f:
-        f.write(gen_css())
-
-    # write js file
-    with open(join(ROOT_DIR, "page", "script.js"), "w") as f:
-        f.write(gen_js())
-    
-    
 def open_website(driver):
     path = join(ROOT_DIR, "page", "index.html")
     driver.get("file:///" + path)
@@ -152,8 +60,8 @@ def main():
         driver = webdriver.Chrome(options=options)
 
         # fetch new data from websites
-        #web.second_hand(driver)
-        #web.subito(driver, last_page=2)
+        web.second_hand(driver, max_elements=10)
+        web.subito(driver, max_elements=10)
         #web.facebook_marketplace(driver)
 
         # generate showcase website, open
