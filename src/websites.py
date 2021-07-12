8355e1865d817009bffa0f049081392cedbec53a
Markus Telser <markus.telser@gmail.com>
2021-07-12 11:06:20 +0200

    write all once, remove first

--- src/websites.py
+++ src/websites.py
@@ -15,11 +15,12 @@ def second_hand(driver, first_page=1, last_page=20, min_price=0, max_price=10000
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="notice"]/div[3]/div[2]/button'))).click()        
     driver.switch_to.default_content()
 
+    stack = [] # save data to write to file
+
     # run through all pages
     noMoreElements = False
     elements = 0
     for i in range(first_page, last_page):
-        first = True
         # stop if there are no more items
         if noMoreElements:
             break
@@ -43,21 +44,23 @@ def second_hand(driver, first_page=1, last_page=20, min_price=0, max_price=10000
                     # print out fetched data, save latest entry/all entrys after that
                     conv_price = int(price.replace('.', '').split("€")[0].split(' ')[0])
                     if conv_price >= min_price and conv_price <= max_price:
-                        if i == first_page and first:
+                        if elements == 0:
                             oldEntry = save_latest_entry("second_hand", link)
                             print("---New Second Hand Items---")
-                            first = False
                         # stop if oldEntry is equal to current entry
                         if oldEntry == link:
                             noMoreElements = True
                             break
                         print(f"{title} || {price} || {location} || {date} || {link}")
-                        write_data("second_hand", title, conv_price, location, date, link)
+                        stack.append([title, conv_price, location, date, link])
                         elements += 1
             except TimeoutException as e:
                 if j == 1:
                     noMoreElements = True
                 break
+    
+    # write them all to file
+    write_data(stack, "second_hand")
 
 def subito(driver, first_page=1, last_page=20, min_price=0, max_price=10000, max_elements=50):
     driver.get("https://www.subito.it")
@@ -65,11 +68,12 @@ def subito(driver, first_page=1, last_page=20, min_price=0, max_price=10000, max
     # accept cookies
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'didomi-notice-agree-button'))).click()
 
+    stack = []  # save data to write to file
+
     # run through all pages
     noMoreElements = False
     elements = 0
     for i in range(first_page, last_page):
-        first = True
         # stop if there are no more items
         if noMoreElements:
             break
@@ -92,21 +96,23 @@ def subito(driver, first_page=1, last_page=20, min_price=0, max_price=10000, max
                     # print out fetched data, save latest entry/all entrys after that
                     conv_price = int(price.replace('.', '').split("€")[0])
                     if conv_price >= min_price and conv_price <= max_price:
-                        if i == first_page and first:
+                        if elements == 0:
                             oldEntry = save_latest_entry("subito", link)
                             print("---New Subito Items---")
-                            first = False
                         # stop if oldEntry is equal to current entry
                         if oldEntry == link:
                             noMoreElements = True
                             break
                         print(f"{title} || {price} || {location} || {date} || {link}")
-                        write_data("subito", title, conv_price, location, date, link)
+                        stack.append([title, conv_price, location, date, link])
                         elements += 1
             except TimeoutException as e:
                 if j == 1:
                     noMoreElements = True
                 break
+
+    # write them all to file
+    write_data(stack, "subito")
 
 def facebook_marketplace(driver, first_element=1, last_element=40, min_price=0, max_price=10000, max_elements=50):
     driver.get("https://www.facebook.com/marketplace")
