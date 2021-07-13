bf655fb5ea4254524068213777ffe8e645f51304
Markus Telser <markus.telser99@gmail.com>
2021-07-13 22:45:36 +0200

    added mileage row

--- src/page.py
+++ src/page.py
@@ -5,6 +5,7 @@ from sys import modules
 
 ROOT_DIR = dirname(dirname(modules['__main__'].__file__))
 
+
 def gen_html():
     html = '<!DOCTYPE html><html>'
     html += '<head><meta http-equiv="Content-Type" content="text/html;charset=utf-8"><title>New Items</title></head>'
@@ -14,12 +15,19 @@ def gen_html():
     if isdir("data"):
         websites = listdir(join(ROOT_DIR, "data", "websites"))
         for i in range(len(websites)):
-            html += '<table class="section">'
-            html += '<tr class="site"><th colspan="4">'+ websites[i].split('.')[0] +'</th></tr>'
+
             with open(join(ROOT_DIR, "data", "websites", websites[i]), "r") as f:
                 lines = f.readlines()
+                html += '<table class="section">'
+                if len(lines) != 0:
+                    html += f'<tr class="site"><th colspan="{len(lines[0].split("||")) - 1}">' + websites[i].split('.')[0] + '</th></tr>'
+                else:
+                    html += f'<tr class="site"><th colspan="4">' + websites[i].split('.')[0] + '</th></tr>'
+
                 html += f'<tr><td class="type">Titel</a></td>'
                 html += f'<td class="type">Price</td>'
+                if len(lines) != 0 and len(lines[0].split("||")) == 6:
+                    html += f'<td class="type">Mileage</td>'
                 html += f'<td class="type">Location</td>'
                 html += f'<td class="type">Date</td></tr>'
                 # add all items
@@ -27,12 +35,15 @@ def gen_html():
                     items = line.split("||")
                     html += f'<tr><td class="items"><a target="_blank" rel="noopener noreferrer" href="{items[4]}">{items[0]}</a></td>'
                     html += f'<td class="items">{items[1]}</td>'
+                    if len(items) == 6:
+                        html += f'<td class="items">{items[5]}</td>'
                     html += f'<td class="items">{items[2]}</td>'
                     html += f'<td class="items">{items[3]}</td>'
                     html += f'<td class="trash"><button onclick="deleteRow(this)"><img src="{join(ROOT_DIR, "data", "trash.svg")}"></button></td></tr>'
-            html += '</table>'
+                html += '</table>'
     html += '</body></html>'
     return html
+
 
 def gen_js():
     js = """
@@ -40,13 +51,6 @@ def gen_js():
         var row = btn.parentNode.parentNode;
         var link = row.getElementsByTagName("td")[0].childNodes[0].getAttribute("href");
         
-        // delete in file
-        let fr = new FileReader();
-        fr.onload = function(e) {
-            // e.target.result should contain the text
-        };
-        fr.readAsText("../data/websites/second_hand.txt");
-
         // remove html column
         row.parentNode.removeChild(row);
     }
@@ -104,6 +108,7 @@ def gen_css():
     """
     return css
 
+
 def gen_website(overwrite=True):
     # write html file
     if overwrite or (not overwrite and not isfile(join(ROOT_DIR, "page", "index.html"))):
@@ -119,4 +124,3 @@ def gen_website(overwrite=True):
     if overwrite or (not overwrite and not isfile(join(ROOT_DIR, "page", "script.js"))):
         with open(join(ROOT_DIR, "page", "script.js"), "w") as f:
             f.write(gen_js())
-    
