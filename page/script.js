d9f5e931bd53eddaf185e959653e3d9c0e7c7295
Markus Telser <markus.telser99@gmail.com>
2021-07-13 23:13:08 +0200

    add example page

--- page/script.js
+++ page/script.js
@@ -1,6 +1,9 @@
 
     function deleteRow(btn) {
         var row = btn.parentNode.parentNode;
+        var link = row.getElementsByTagName("td")[0].childNodes[0].getAttribute("href");
+        
+        // remove html column
         row.parentNode.removeChild(row);
     }
     
